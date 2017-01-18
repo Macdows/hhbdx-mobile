913392623bff0c3c1bb49e93abb6d89aa1d5a1b6
Thomas Morgana <tmorgana64@gmail.com>
2017-01-18 14:03:44 +0100

    Display popover above everithing

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,15 +55,125 @@ angular.module('starter.controllers', ['ionic'])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-  
-  $ionicLoading.show({
+.controller('MapCtrl', function($scope, $ionicLoading, $compile, $ionicPopup, $ionicPopover, ClosePopupService) {
+
+  /*$ionicLoading.show({
     content: 'Loading',
     animation: 'fade-in',
     showBackdrop: true,
     maxWidth: 200,
     showDelay: 0
+  });*/
+
+  $scope.showFilter = false;
+  $scope.distMax = 2500;
+
+  $scope.filters = {
+    beer: false,
+    wine: false,
+    cocktail: false
+  };
+
+  $scope.pubs = [{
+    id: 1,
+    name: "Mushroom",
+    distance: 450,
+    location: "Gambetta",
+    happyStart: 17,
+    happyEnd: 20,
+    price: 3.50,
+    image: "img/mushroom.jpg",
+    mainDrinkType: "Beer",
+    secondDrinkType: "Cocktail",
+    rating: [4.2]
+  }, {
+    id: 2,
+    name: "Camelot",
+    distance: 1535,
+    location: "La Victoire",
+    happyStart: 17.30,
+    happyEnd: 19,
+    price: 3.50,
+    image: "img/camelot.jpg",
+    mainDrink: "Cocktail",
+    secondDrinkType: "Beer",
+    rating: [3.8]
+  }, {
+    id: 3,
+    name: "Titi Twister",
+    distance: 1475,
+    location: "La Victoire",
+    happyStart: 18,
+    happyEnd: 21,
+    price: 3.50,
+    image: "img/titi.jpg",
+    mainDrink: "Beer",
+    secondDrinkType: "Wine",
+    rating: [4.6, 3.9]
+  },
+  {
+    id: 1,
+    name: "Mushroom",
+    distance: 450,
+    location: "Gambetta",
+    happyStart: 17,
+    happyEnd: 20,
+    price: 3.50,
+    image: "img/mushroom.jpg",
+    mainDrinkType: "Beer",
+    secondDrinkType: "Cocktail",
+    rating: [4.2]
+  }, {
+    id: 2,
+    name: "Camelot",
+    distance: 1535,
+    location: "La Victoire",
+    happyStart: 17.30,
+    happyEnd: 19,
+    price: 3.50,
+    image: "img/camelot.jpg",
+    mainDrink: "Cocktail",
+    secondDrinkType: "Beer",
+    rating: [3.8]
+  }, {
+    id: 3,
+    name: "Titi Twister",
+    distance: 1475,
+    location: "La Victoire",
+    happyStart: 18,
+    happyEnd: 21,
+    price: 3.50,
+    image: "img/titi.jpg",
+    mainDrink: "Beer",
+    secondDrinkType: "Wine",
+    rating: [4.6, 3.9]
+  }];
+
+  $ionicPopover.fromTemplateUrl('templates/popover.html', {
+      scope: $scope,
+    }).then(function(popover) {
+
+      console.log("coucou");
+      $scope.popover = popover;
+    });
+
+  // Triggered on a button click, or some other target
+  $scope.showInfo = function() {
+  $scope.data = {};
+
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    templateUrl: "templates/popup.html",
+    title: 'Bars',
+    cssClass: 'barsPopup',
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel' }
+    ]
   });
+  ClosePopupService.register(myPopup);
+  };
+
 
   navigator.geolocation.getCurrentPosition(function(pos) {
     var myLatlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
@@ -82,7 +192,7 @@ angular.module('starter.controllers', ['ionic'])
            map: map
        });
        $scope.map = map;
-       $ionicLoading.hide();
+       //$ionicLoading.hide();
      }, function(error) {
        alert('Unable to get location: ' + error.message);
      });
